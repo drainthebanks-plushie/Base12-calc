0737085b7cd2e2fb5fd2707145e13d4e9a4d1057
drainthebanks-plushie <drainthebanks@gmail.com>
2026-08-28 23:33:51 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "base12-kaktovik-v1";
+const CACHE = "base12-kaktovik-v5";
 const ASSETS = [
   "./",
   "./index.html",
@@ -24,6 +24,10 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request))
   );
 });
